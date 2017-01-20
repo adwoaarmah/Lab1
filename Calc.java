ced6656410ef375a844110ac677c9d5abb2ae53a
adwoaarmah <adwoa.armah@ashesi.edu.gh>
2017-01-20 17:36:36 +0000

    I have committed

---
+++ Calc.java
@@ -0,0 +1,15 @@
+
+public class Calc;
+{
+public static void main(String[]args)
+{
+	double input1, input2;
+	if (args[0].equals("add")){
+		input1=Double.parseDouble(args[1]);
+		input2=Double.parseDouble(args[2]);
+
+
+	System.out.println(input1+input2);
+	}
+}
+}
