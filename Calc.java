d8bd9d54b32c7a142d8baac881929c9820511cae
adwoaarmah <adwoa.armah@ashesi.edu.gh>
2017-01-23 13:59:45 +0000

    Subtracted

--- Calc.java
+++ Calc.java
@@ -11,7 +11,6 @@ public static void main(String[]args)
 
 	System.out.println(input1+input2);
 	}
-	
 	else if (args[0].equals("subtract")){
 		input1=Double.parseDouble(args[1]);
 		input2=Double.parseDouble(args[2]);
